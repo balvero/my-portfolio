fb8970b74e50f4a3d0ef1adb6412716fc0a8ab8e
Bernard Alvero <bernard.alvero@gmail.com>
2017-04-26 11:17:23 +0800

    Implemented pagination | Blog mobile fix

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -6,7 +6,7 @@ class BlogsController < ApplicationController
   # GET /blogs
   # GET /blogs.json
   def index
-    @blogs = Blog.all.order('created_at DESC')
+    @blogs = Blog.page(params[:page]).per(5)
     # @blogs = Blog.featured_blog
     @page_title = "My Blog Posts"
   end
