328c5f87b1a2ed77ee192ad5705fc83daea3867d
Bernard Alvero <bernard.alvero@gmail.com>
2017-08-18 15:48:53 +0800

    Refactored alerts into helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -72,4 +72,16 @@ module ApplicationHelper
     "active" if current_page? path
   end
 
+  def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+
+    if alert
+      alert_generator alert
+    end
+  end
+
+  def alert_generator msg
+    js add_gritter(msg, title: "Noobita", sticky: false, time: 5000)
+  end
+
 end
