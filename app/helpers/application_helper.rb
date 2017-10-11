90f56bd97341f989649f2f26fd3d1998135ee456
Bernard Alvero <bernard.alvero@gmail.com>
2017-10-11 17:09:40 +0800

    Added markdown support for Portfolio

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,19 +20,24 @@ module ApplicationHelper
     NoobitaViewTool::Renderer.copyright 'Noobita', 'All rights reserved'
   end
 
-  def bootstrap_class_for(flash_type)
-    case flash_type
-      when 'success'
-        "alert-success"
-      when 'error'
-        "alert-error"
-      when 'alert'
-        "alert-warning"
-      when 'notice'
-        "alert-info"
-      else
-        flash_type
+  class CodeRayify < Redcarpet::Render::HTML
+    def block_code(code, language)
+      CodeRay.scan(code, language).div
     end
+  end
+
+  def markdown(text)
+    coderayified = CodeRayify.new(filter_html: true, hard_wrap: true)
+
+    options = {
+        fenced_code_blocks: true,
+        no_intra_emphasis: true,
+        autolink: true,
+        lax_html_blocks: true
+    }
+
+    markdown_to_html = Redcarpet::Markdown.new(coderayified, options)
+    markdown_to_html.render(text).html_safe
   end
 
   def nav_items
